d65cfe3e7131a28c2e3ebc7041c0f3ef3c8a2607
Harold Trinidad <91819411+harrypaps@users.noreply.github.com>
2024-07-30 19:49:52 +0800

    script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Register the service worker
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/service-worker.js')
+        navigator.serviceWorker.register('service-worker.js')
             .then(registration => {
                 console.log('ServiceWorker registration successful with scope: ', registration.scope);
             })
